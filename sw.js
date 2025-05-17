7dc4a7b3f5261feb139b5bce6dfd07c1fa6415d7
MMSSB <56227348+MMSSB@users.noreply.github.com>
2025-05-17 15:34:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'mms-portfolio-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/script.js',
+  '/assets/images/mmslogo.PNG',
+  '/assets/images/mms.jpg',
+  '/assets/images/egypt-flag-icon.svg',
+  // Add paths to all your other essential assets here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
